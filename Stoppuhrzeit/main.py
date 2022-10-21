ce1b363b70ba619f4e099510bfb7a67e5629c530
Quinn-Quantum <79361383+Quinn-Quantum@users.noreply.github.com>
2022-10-21 14:09:56 +0200

    add a Layouer

--- Stoppuhrzeit/main.py
+++ Stoppuhrzeit/main.py
@@ -1,20 +1,31 @@
+import random
+
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 
+red = [1,0,0,1]
+green = [0,1,0,1]
+blue =  [0,0,1,1]
+purple = [1,0,1,1]
 
 
 class MainApp(App):
     def build(self):
-        label_hallo = Label(text='Hello User',
-                            size_hint=(.10, .10),
-                            pos_hint={'center_x': .5, 'center_y': .5},
-                            color='yellow')
+        layout = BoxLayout(padding=10)
+        colors = [red, green, blue, purple]
 
-        button1 = Button(text="Bild analysieren", size_hint=(.5, .10), pos_hint={'center_x': .5, 'center_y': .5},
-                         color='yellow', background_color='blue')
+        for i in range(5):
+            btn = Button(text="Button #%s" % (i + 1),
+                         background_color=random.choice(colors),
+                         size_hint=(.5, .5)
+                         )
 
-        return button1
+            layout.add_widget(btn)
+        label = Label(text = "Hallo Welt",size_hint=(.5, .5),pos_hint={'center_x': 0, 'center_y': 0}, color = "yellow" )
+        layout.add_widget(label)
+        return layout
 
 
 if __name__ == '__main__':
