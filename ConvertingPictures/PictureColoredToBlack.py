f3c704fc9750c5e0e0c0d5752e780a30ab268921
Gökmen Demir <magia_destructor@gmx.de>
2022-11-24 14:11:06 +0100

    Fix GreyScaling ,Change Save Area to be Downloads

--- ConvertingPictures/PictureColoredToBlack.py
+++ ConvertingPictures/PictureColoredToBlack.py
@@ -1,9 +1,13 @@
 from PIL import Image
+from pathlib import Path
 
 class PictureColoredToBlack():
     
-    def ChangeToBlackWhite(picture):
-        picture_to_black_and_white = Image.open(picture).convert('L')
+    def ChangeToBlackWhite(self,picture):
+        path = "./LF12Sportverein_Stoppuhrzeit/ConvertingPictures/"
+        downloads_path = str(Path.home() / "Downloads")
 
-        picture_to_black_and_white.save('greyscaled_'+ picture)
+        picture_to_black_and_white = Image.open(path+ picture).convert('L')
+
+        picture_to_black_and_white.save(downloads_path +'/'+'greyscaled_'+ picture)
         
