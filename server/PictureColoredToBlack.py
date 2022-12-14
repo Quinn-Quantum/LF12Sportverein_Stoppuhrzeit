b5476916b45db34e1bae0a9cde4a60b46a27980f
Gökmen Demir <magia_destructor@gmx.de>
2022-12-14 15:41:06 +0100

    updated Pathing

--- server/PictureColoredToBlack.py
+++ server/PictureColoredToBlack.py
@@ -5,11 +5,14 @@ class PictureColoredToBlack():
     #This Method changes the selected picture into a greyscaled version 
     def ChangeToBlackWhite(self,picture):
         #sets the Path of the Project to access the image
-        path = "./LF12Sportverein_Stoppuhrzeit/ConvertingPictures/"
+        #pathPicture = "." + path
         #sets the Download path where to safe the changed picture
         downloads_path = str(Path.home() / "Downloads")
         #Converts the selected picture into a greyscaled
-        picture_to_black_and_white = Image.open(path+ picture).convert('L')
+        picture_to_black_and_white = Image.open(picture).convert('L')
+        splitted = picture.split("\\")
+        imageName = splitted[len(splitted)-1]
         #Saves the picture into the Download Path
-        picture_to_black_and_white.save(downloads_path +'/'+'greyscaled_'+ picture)
+        picture_to_black_and_white.save(downloads_path +'/'+'greyscaled_'+ imageName)
         
+PictureColoredToBlack().ChangeToBlackWhite(".\LF12Sportverein_Stoppuhrzeit\kaneki.jpg");
