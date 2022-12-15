a5d9f750307d8cd15b1edd4ae2a61a8f0aa77d59
Quinn-Quantum <79361383+Quinn-Quantum@users.noreply.github.com>
2022-12-15 14:32:56 +0100

    Lode Button with funktion

--- Stoppuhrzeit/main.py
+++ Stoppuhrzeit/main.py
@@ -1,13 +1,8 @@
 from kivy.app import App
 from kivy.factory import Factory
 from kivy.properties import ObjectProperty
-from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.popup import Popup
-from kivy.uix.textinput import TextInput
 import os
 
 #https://www.youtube.com/watch?v=YlRd4rw_vBw
@@ -22,22 +17,15 @@ class LoadDialog(FloatLayout):
     load = ObjectProperty(None)
     cancel = ObjectProperty(None)
     selected = ObjectProperty(None)
+
     #function shows the pricture in the app and get the file
     def selected(self, filename):
         try:
             self.ids.my_image.source = filename[0]
             print(filename[0])
 
-            #picture colored to black and whith
-            PictureColoredToBlack.PictureColoredToBlack.ChangeToBlackWhite(self,filename[0])
-           
         except:
             pass
-
-#Class for the <SaveDialog> in the editor.kv
-class SaveDialog(FloatLayout):
-    text_input = ObjectProperty(None)
-    cancel = ObjectProperty(None)
 
 
 class Root(FloatLayout):
@@ -48,8 +36,6 @@ class Root(FloatLayout):
     def dismiss_popup(self):
         self._popup.dismiss()
 
-
-
     #open data in a new window
     def show_load(self):
         content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
@@ -57,26 +43,19 @@ class Root(FloatLayout):
                             size_hint=(0.9, 0.9))
         self._popup.open()
 
-    def show_save(self):
-        content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
-        self._popup = Popup(title="Save file", content=content,
-                            size_hint=(0.9, 0.9))
-        self._popup.open()
-    #init the Picture
-    def load(self, path, filename):
-        with open(os.path.join(path, filename[0])) as stream:
-            self.text_input.text = stream.read()
+    #aktiviert die funktion zum schwarz weiß machen
+    def load(self, filename):
 
+        # picture colored to black and whith
+        PictureColoredToBlack.PictureColoredToBlack.ChangeToBlackWhite(self, filename[0])
+        #Schlißt Verzeichniss
         self.dismiss_popup()
 
-    def save(self, path, filename):
-        with open(os.path.join(path, filename), 'w') as stream:
-            stream.write(self.text_input.text)
 
-        self.dismiss_popup()
 
-    def test(self):
-        print("Hallo")
+
+
+
 
 
 #Start point
@@ -89,7 +68,6 @@ class Editor(App):
 #Dialog definition in editor.kv
 Factory.register('Root', cls=Root)
 Factory.register('LoadDialog', cls=LoadDialog)
-Factory.register('SaveDialog', cls=SaveDialog)
 
 if __name__ == '__main__':
     app = Editor()
