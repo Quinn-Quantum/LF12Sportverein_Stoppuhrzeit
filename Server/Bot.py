533b049a4a955982cfb3c3bcbec591d518109884
jeetdheri <gurjitdheri@gmail.com>
2022-12-15 16:07:26 +0100

    Stopuhr bilder

--- Server/Bot.py
+++ Server/Bot.py
@@ -1,21 +1,16 @@
 from PIL import Image
 from pytesseract import pytesseract
 
+class getText:
 #Define path to tessaract.exe
-path_to_tesseract = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+    path_to_tesseract = '\Tesseract-OCR\tesseract.exe'
+    #Define path to image
+    path_to_image = 'file:///Users/Gurjit/Downloads/istockphoto-641719442-612x612.jpg'
+    #Point tessaract_cmd to tessaract.exe
+    pytesseract.tesseract_cmd = path_to_tesseract
+    #Open image with PIL
 
-#Define path to image
-path_to_image = 'image\sampletext1-ocr.png'
-
-#Point tessaract_cmd to tessaract.exe
-pytesseract.tesseract_cmd = path_to_tesseract
-
-#Open image with PIL
-img = Image.open(path_to_image)
-
-#Extract text from image
-text = pytesseract.image_to_string(img)
-
-print(text)
-
-
+    img = Image.open(path_to_image)
+    #Extract text from image
+    text = pytesseract.image_to_string(img)
+    print(text)
