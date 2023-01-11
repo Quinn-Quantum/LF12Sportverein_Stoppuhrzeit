8cde3f05069aed77d5617a7f232b84b7531fc075
Quinn-Quantum <79361383+Quinn-Quantum@users.noreply.github.com>
2023-01-11 14:38:43 +0100

    merge

--- Server/PictureColoredToBlack.py
+++ Server/PictureColoredToBlack.py
@@ -2,12 +2,15 @@ from PIL import Image
 from pathlib import Path
 
 class PictureColoredToBlack():
-    
-    def ChangeToBlackWhite(self,picture):
-        path = "./LF12Sportverein_Stoppuhrzeit/ConvertingPictures/"
+    #This Method changes the selected picture into a greyscaled version 
+    #and saves the modified picture in the Downloads Folder
+    def ChangeToBlackWhite(self,picture):  
+        #sets the Download path where to safe the changed picture
         downloads_path = str(Path.home() / "Downloads")
-
-        picture_to_black_and_white = Image.open(path+ picture).convert('L')
-
-        picture_to_black_and_white.save(downloads_path +'/'+'greyscaled_'+ picture)
+        #Modifies the Selected Picture into a greyscaled version
+        picture_to_black_and_white = Image.open(picture).convert('L')
+        splitted = picture.split("\\")
+        imageName = splitted[len(splitted)-1]
+        #Saves the picture into the Download Folder 
+        picture_to_black_and_white.save(downloads_path +'/'+'greyscaled_'+ imageName[:-3] + "png")
         
