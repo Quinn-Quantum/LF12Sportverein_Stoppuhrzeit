c3f192b8c55066c4b58826929d3e09897da0c16a
jeetdheri <gurjitdheri@gmail.com>
2023-01-12 12:42:22 +0100

    Bot erneuert

--- Server/Bot.py
+++ Server/Bot.py
@@ -2,15 +2,16 @@ from PIL import Image
 from pytesseract import pytesseract
 
 class getText:
-#Define path to tessaract.exe
-    path_to_tesseract = '\Tesseract-OCR\tesseract.exe'
-    #Define path to image
-    path_to_image = 'file:///Users/Gurjit/Downloads/istockphoto-641719442-612x612.jpg'
-    #Point tessaract_cmd to tessaract.exe
-    pytesseract.tesseract_cmd = path_to_tesseract
-    #Open image with PIL
+    def extract_text_from_image(image_path):
+        # Laden des Bildes
+        image = Image.open(image_path)
 
-    img = Image.open(path_to_image)
-    #Extract text from image
-    text = pytesseract.image_to_string(img)
-    print(text)
+        #Verwendung von pytesseract, um Text aus dem Bild zu extrahieren
+        text = pytesseract.image_to_string(image)
+
+        #Ausgabe des extrahieren Texts
+        print(text)
+
+    #Beispieleaufruf TODO
+    extract_text_from_image("/Users/Gurjit/LF12Sportverein_Stoppuhrzeit/image")
+   
